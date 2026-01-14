640d5b00dd1ad6ecb96afb2c7db5e084253a7237
Shun Ueda <me@shu.nu>
2026-01-14 11:25:49 -0500

    ci: add ci

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -5,13 +5,28 @@
     {
       treefmt = {
         programs = {
-          keep-sorted = {
-            enable = true;
-          };
+          # keep-sorted start block=yes
+          autocorrect.enable = true;
+          keep-sorted.enable = true;
           nixfmt = {
             enable = true;
             strict = true;
           };
+          typos.enable = true;
+          yamlfmt = {
+            enable = true;
+            settings.formatter = {
+              type = "basic";
+              # allow single empty line
+              retain_line_breaks_single = true;
+              # https://github.com/google/yamlfmt/issues/84
+              scan_folded_as_literal = true;
+              # according to the doc - "crlf on Windows, lf otherwise". Explicitly setting
+              # to avoid inconsistency.
+              line_ending = "lf";
+            };
+          };
+          # keep-sorted end
         };
       };
     }
