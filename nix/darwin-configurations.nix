ad36a4f7562b4a3457f86a01d69abaf5157c1516
Shun Ueda <me@shu.nu>
2026-01-26 20:56:17 -0500

    refactor: use home-manager sops-nix

--- nix/darwin-configurations.nix
+++ nix/darwin-configurations.nix
@@ -19,7 +19,6 @@
               {
                 imports = [
                   inputs.home-manager.darwinModules.home-manager
-                  inputs.sops-nix.darwinModules.sops
                   darwinModules.pin-nixpkgs
                 ];
                 nix = {
@@ -67,156 +66,158 @@
                   _1password.enable = true;
                 };
                 security.pam.services.sudo_local.touchIdAuth = true;
-                home-manager.users.${user} = {
-                  imports = [ ../programs/homerow.nix ];
-                  xdg.enable = true;
-                  programs = {
-                    # keep-sorted start block=yes
-                    bash = {
-                      enable = true;
-                      historyControl = [
-                        "ignorespace"
-                        "ignoredups"
-                      ];
-                      historySize = 1000000;
-                      historyFileSize = 1000000;
-                    };
-                    docker-cli.enable = true;
-                    emacs = {
-                      enable = true;
-                      extraPackages =
-                        epkgs: with epkgs; [
-                          avy
-                          corfu
-                          editorconfig
-                          exec-path-from-shell
-                          fzf
-                          git-gutter
-                          magit
-                          nix-ts-mode
-                          ocaml-eglot
-                          treesit-grammars.with-all-grammars
-                          tuareg
-                          undo-tree
-                          vertico
-                          zenburn-theme
-                        ];
-                    };
-                    fzf.enable = true;
-                    ghostty = {
-                      enable = true;
-                      package = pkgs.ghostty-bin;
-                      settings = {
-                        auto-update = "off";
-                        link-previews = true;
-                        mouse-hide-while-typing = true;
-                        window-save-state = "always";
-                      };
-                    };
-                    git = {
-                      enable = true;
-                      settings = {
-                        init = {
-                          defaultBranch = "master";
-                        };
-                        user = {
-                          name = "Shun Ueda";
-                          email = "me@shu.nu";
-                        };
-                        diff.algorithm = "histogram";
-                        rebase = {
-                          autosquash = true;
-                          autostash = true;
-                          stat = true;
-                        };
-                        merge.directoryRenames = true;
-                        rerere = {
-                          autoupdate = true;
-                          enabled = true;
-                        };
-                      };
-                      hooks.pre-commit = lib.getExe inputs.nocommit.packages.${system}.default;
-                    };
-                    home-manager.enable = true;
-                    homerow.enable = true;
-                    mergiraf.enable = true;
-                    ssh = {
-                      enable = true;
-                      enableDefaultConfig = false;
-                      matchBlocks = {
-                        "*" = {
-                          identityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
-                        };
-                        "github.com" = {
-                          user = "git";
-                        };
-                        "codeberg.org" = {
-                          user = "git";
-                        };
-                      };
-                    };
-                    starship = {
-                      enable = true;
-                      settings = {
-                        add_newline = false;
-                        format = "$git_branch:$directory $character ";
-                        character = {
-                          format = "[\\$](white)";
-                        };
-                        directory = {
-                          format = "[$path]($style)";
-                          style = "bold blue";
-                        };
-                        git_branch = {
-                          format = "[$branch]($style)";
-                          style = "bold green";
-                        };
-                      };
-                    };
-                    zoxide.enable = true;
-                    # keep-sorted end
-                  };
-                  fonts.fontconfig.enable = true;
-                  home = {
-                    username = user;
-                    stateVersion = "25.11";
-                    packages = with pkgs; [
-                      age
-                      gnupg
-                      jetbrains-mono
-                      maccy
-                      orbstack
-                      nixd
-                      ocamlPackages.ocaml-lsp
-                      sops
-                      typescript
-                      typescript-language-server
+                home-manager.users.${user} =
+                  { config, ... }:
+                  {
+                    imports = [
+                      ../programs/homerow.nix
+                      inputs.sops-nix.homeManagerModules.sops
                     ];
-                    file = {
-                      ".emacs.d" = {
-                        source = ../.emacs.d;
-                        recursive = true;
+                    xdg.enable = true;
+                    programs = {
+                      # keep-sorted start block=yes
+                      bash = {
+                        enable = true;
+                        historyControl = [
+                          "ignorespace"
+                          "ignoredups"
+                        ];
+                        historySize = 1000000;
+                        historyFileSize = 1000000;
                       };
-                      ".hushlogin" = {
-                        text = "";
+                      docker-cli.enable = true;
+                      emacs = {
+                        enable = true;
+                        extraPackages =
+                          epkgs: with epkgs; [
+                            avy
+                            corfu
+                            editorconfig
+                            exec-path-from-shell
+                            fzf
+                            git-gutter
+                            magit
+                            nix-ts-mode
+                            ocaml-eglot
+                            treesit-grammars.with-all-grammars
+                            tuareg
+                            undo-tree
+                            vertico
+                            zenburn-theme
+                          ];
+                      };
+                      fzf.enable = true;
+                      ghostty = {
+                        enable = true;
+                        package = pkgs.ghostty-bin;
+                        settings = {
+                          auto-update = "off";
+                          link-previews = true;
+                          mouse-hide-while-typing = true;
+                          window-save-state = "always";
+                        };
+                      };
+                      git = {
+                        enable = true;
+                        settings = {
+                          init = {
+                            defaultBranch = "master";
+                          };
+                          user = {
+                            name = "Shun Ueda";
+                            email = "me@shu.nu";
+                          };
+                          diff.algorithm = "histogram";
+                          rebase = {
+                            autosquash = true;
+                            autostash = true;
+                            stat = true;
+                          };
+                          merge.directoryRenames = true;
+                          rerere = {
+                            autoupdate = true;
+                            enabled = true;
+                          };
+                        };
+                        hooks.pre-commit = lib.getExe inputs.nocommit.packages.${system}.default;
+                      };
+                      home-manager.enable = true;
+                      homerow.enable = true;
+                      mergiraf.enable = true;
+                      ssh = {
+                        enable = true;
+                        enableDefaultConfig = false;
+                        matchBlocks = {
+                          "*" = {
+                            identityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+                          };
+                          "github.com" = {
+                            user = "git";
+                          };
+                          "codeberg.org" = {
+                            user = "git";
+                          };
+                        };
+                      };
+                      starship = {
+                        enable = true;
+                        settings = {
+                          add_newline = false;
+                          format = "$git_branch:$directory $character ";
+                          character = {
+         g                   format = "[\\$](white)";
+                          };
+                          directory = {
+                            format = "[$path]($style)";
+                            style = "bold blue";
+                          };
+                          git_branch = {
+                            format = "[$branch]($style)";
+                            style = "bold green";
+                          };
+                        };
+                      };
+                      zoxide.enable = true;
+                      # keep-sorted end
+                    };
+                    fonts.fontconfig.enable = true;
+                    home = {
+                      username = user;
+                      stateVersion = "25.11";
+                      packages = with pkgs; [
+                        age
+                        gnupg
+                        jetbrains-mono
+                        maccy
+                        orbstack
+                        nixd
+                        ocamlPackages.ocaml-lsp
+                        sops
+                        typescript
+                        typescript-language-server
+                      ];
+                      file = {
+                        ".emacs.d" = {
+                          source = ../.emacs.d;
+                          recursive = true;
+                        };
+                        ".hushlogin" = {
+                          text = "";
+                        };
                       };
                     };
-                    sessionVariable = {
-                      EDITOR = "emacs";
+                    sops = {
+                      age = {
+                        keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+                        sshKeyPaths = [ ];
+                      };
+                      gnupg.sshKeyPaths = [ ];
+                      defaultSopsFile = ../secrets/example.yaml;
+                      secrets.example_key = {
+                        path = "${config.home.homeDirectory}/test.txt";
+                      };
                     };
                   };
-                };
-                sops = {
-                  age = {
-                    keyFile = "/Users/${user}/.config/sops/age/keys.txt";
-                    sshKeyPaths = [ ];
-                  };
-                  gnupg.sshKeyPaths = [ ];
-                  defaultSopsFile = ../secrets/example.yaml;
-                  secrets.example_key = {
-                    path = "/Users/${user}/test.txt";
-                  };
-                };
               };
             linux-builder =
               { lib, ... }:
